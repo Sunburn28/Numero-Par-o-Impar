166c21f434cca057db351fbc0127334a5100dc99
anael <anael@BOREDGUY28>
2024-02-13 18:39:36 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Pastrana Garnica Anael Alejandro (226Z0188)
+//13-feb-2024
+
+using System.Drawing;
+int numero;
+
+Console.WriteLine("Dame un numero: ");
+numero = int.Parse(Console.ReadLine());
+
+
+if(numero % 2 == 0)
+{
+    Console.WriteLine("El numero es par");
+}
+else
+{
+    Console.WriteLine("El numero es impar");
+}
